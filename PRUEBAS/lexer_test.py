0372d23958291e9739e127a274c443915f5615f1
cayoss <cayoss.77@gmail.com>
2022-07-17 16:04:08 -0500

    Primera version del análisis léxico

---
+++ PRUEBAS/lexer_test.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+class LexerTest(TestCase):
+
+    def test_illegal(self) -> None:
+        source: str = '¡¿@'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(len(source)):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.ILLEGAL, '¡'),
+            Token(TokenType.ILLEGAL, '¿'),
+            Token(TokenType.ILLEGAL, '@'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
